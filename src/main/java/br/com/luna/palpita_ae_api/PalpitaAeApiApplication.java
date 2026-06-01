6b2012ee7a73ede4dc36a48fe4d1fc631d19a945
luna.as <luna.as@itaipuparquetec.org.br>
2026-06-01 15:40:58 -0300

    chore: initial commit

---
+++ src/main/java/br/com/luna/palpita_ae_api/PalpitaAeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.luna.palpita_ae_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalpitaAeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalpitaAeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luna/palpita_ae_api/PalpitaAeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luna.palpita_ae_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalpitaAeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
