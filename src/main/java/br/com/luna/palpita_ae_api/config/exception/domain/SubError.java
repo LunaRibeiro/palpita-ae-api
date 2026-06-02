21662d9d746959ef1ae899db1c00342977664f69
luna.as <luna.as@itaipuparquetec.org.br>
2026-06-02 15:34:19 -0300

    feat: implement global exception handling with custom exception classes and error response structures

---
+++ src/main/java/br/com/luna/palpita_ae_api/config/exception/domain/SubError.java
@@ -0,0 +1,4 @@
+package br.com.luna.palpita_ae_api.config.exception.domain;
+
+public interface SubError {
+}
---
+++ src/main/java/br/com/luna/palpita_ae_api/config/exception/types/EntityNotFoundException.java
@@ -0,0 +1,23 @@
+package br.com.luna.palpita_ae_api.config.exception.types;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EntityNotFoundException extends RuntimeException{
+    private final String entity;
+    private final Long id;
+
+    public EntityNotFoundException(String entity, Long id) {
+        super();
+        this.entity = entity;
+        this.id = id;
+    }
+
+    public EntityNotFoundException(String entity) {
+        super();
+        this.entity = entity;
+        this.id = null;
+    }
+}
