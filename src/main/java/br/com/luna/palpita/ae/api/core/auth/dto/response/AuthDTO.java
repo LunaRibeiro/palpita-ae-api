7448ab242f64ecb789887cfb1761a7fb2608200b
luna.as <luna.as@itaipuparquetec.org.br>
2026-06-03 16:54:35 -0300

    refactor: remove unused AuthDTO, LoginFormDTO, and RegisterFormDTO

--- src/main/java/br/com/luna/palpita/ae/api/core/auth/dto/response/AuthDTO.java
+++
@@ -1,9 +0,0 @@
-package br.com.luna.palpita.ae.api.core.auth.dto.response;
-
-import br.com.luna.palpita.ae.api.core.profile.domain.dto.response.ProfileDTO;
-
-public record AuthDTO(
-        String token,
-        ProfileDTO profile
-) {
-}
